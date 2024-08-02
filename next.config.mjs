aaef69a32b4552de70db8b02cd7a96547744c5c6
Ahmed Sharif Khan <abdunnoor540@gmail.com>
2024-08-03 02:09:18 +0600

    ss

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    env: {
-      NOTION_API_KEY: process.env.NOTION_API_KEY,
-      NOTION_DATABASE_ID: process.env.NOTION_DATABASE_ID,
-    },
-  };
-  
-  export default nextConfig;
-  
+  env: {
+    NOTION_API_KEY: process.env.NOTION_API_KEY,
+    NOTION_DATABASE_ID: process.env.NOTION_DATABASE_ID,
+  },
+  output: 'export',
+  trailingSlash: true,
+};
+
+export default nextConfig;
